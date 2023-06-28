3597669600a40e578a1fbb44640127e00a15d91e
Vinay-BC <vinaybc.xworkz@gmail.com>
2023-06-28 10:20:24 +0530

    Hi

---
+++ Bike.java
@@ -0,0 +1,9 @@
+public class Bike {
+    public static void main(String[] args){
+        System.out.println("Green colour Bike");
+        System.out.println("2 seats in the Bike");
+        System.out.println("Ktm Bike");
+        System.out.println("Duke Bike");
+        System.out.println("1 seat or 2 seat AC Bike");
+    }   
+}
