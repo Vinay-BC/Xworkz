d0b0556b65d8f57192e8c04a08561dd72840e779
Vinay-BC <vinaybc.xworkz@gmail.com>
2023-06-29 12:26:51 +0530

    MobileRunner

---
+++ MobileRunner.java
@@ -0,0 +1,16 @@
+class MobileRunner
+{
+	public static void main(String[] args)
+	{
+		Mobile mobile=new Mobile();
+		Mobile name=new Mobile();
+		Mobile modelname=new Mobile();
+		Mobile model_id=new Mobile();
+		Mobile batterybackup=new Mobile();
+		Mobile cost=new Mobile();
+		Mobile inches=new Mobile();
+		Mobile ram=new Mobile();
+		Mobile processor=new Mobile();
+		Mobile camerapixcel=new Mobile();
+	}
+}
