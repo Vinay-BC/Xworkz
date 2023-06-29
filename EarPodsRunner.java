3c5c868b79da1c52b663ce90b5dfb2bf2379e63c
Vinay-BC <vinaybc.xworkz@gmail.com>
2023-06-29 12:24:21 +0530

    EarPodsRuunner

---
+++ EarPodsRunner.java
@@ -0,0 +1,19 @@
+class EarPodsRunner
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Running main method");
+		EarPods earpods=new EarPods();
+		EarPods name=new EarPods();
+		EarPods types=new EarPods();
+		EarPods design=new EarPods();
+		EarPods cose=new EarPods();
+		EarPods sensitive=new EarPods();
+		EarPods connectivity=new EarPods();
+		EarPods controls=new EarPods();
+		EarPods brand=new EarPods();
+		EarPods noiseCancellation=new EarPods();
+		EarPods frequencyResponse=new EarPods();
+		EarPods volume=new EarPods();
+	}
+}
