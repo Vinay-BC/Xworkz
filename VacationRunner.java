790ead2ff1657cbd54d710fa793ff5390d9de86b
Vinay-BC <vinaybc.xworkz@gmail.com>
2023-06-29 12:21:44 +0530

    VacationRunner

---
+++ VacationRunner.java
@@ -0,0 +1,29 @@
+class VacationRunner {
+	public static void main(String[] Bake)
+	{
+		Vacation Tour1=new Vacation();
+		System.out.println("I went on a vacation to beach");
+		Vacation Tour2=new Vacation();
+		System.out.println("I enjoyed swimming in the ocean");
+		Vacation Tour3=new Vacation();
+		System.out.println("We took Family vacation to a theme park");
+		Vacation Tour4=new Vacation();
+		System.out.println("Road exciting roller coasters");
+		Vacation Tour5=new Vacation();
+		System.out.println("My parents planned family vacation to the mountains");
+		Vacation Tour6=new Vacation();
+		System.out.println("I had a fun vacation with my friends at a lake");
+		Vacation Tour7=new Vacation();
+		System.out.println("We went for Fishing and boating in the lake");
+		Vacation Tour8=new Vacation();
+		System.out.println("We went for historical places in Karnataka for the vacation");
+		Vacation Tour9=new Vacation();
+		System.out.println("We went to Ancient temple to visit for our vacation with friends");
+		Vacation Tour10=new Vacation();
+		System.out.println("We went to Dharmastala temple for our last vacation");
+		Vacation Tour11=new Vacation();
+		System.out.println("i love going to different places and Study their culture..");
+		
+		
+	}
+}
